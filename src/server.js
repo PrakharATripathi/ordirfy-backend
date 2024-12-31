5df3d5684025aac3df9cb319d68b4285cd22582a
code-by-Prakhar <151377167+PrakharATripathi@users.noreply.github.com>
2024-12-31 22:03:02 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -16,6 +16,12 @@ app.use(cors({ origin: '*' }));
 
 app.use('/', apiRoutes());
 
+
+
+app.get('/',(req,res)=>{
+    res.send('this is orderidy backend');
+})
+
 app.use('*', (req, res) => {
     res.status(404).send('404 Not found');
 });
